23f6792c62f48349bd3b40482ff9c4ff56bdd4ba
uwek <Uwe.Kallmeyer@gmail.com>
2021-04-09 17:15:48 +0200

    chg

--- js/app.js
+++ js/app.js
@@ -17,3 +17,42 @@ Opportunity
 
 
 */
+
+function myFunction(event) {
+  event.preventDefault();
+
+  var x = document.getElementById("myDIV");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function formToJSON(elements) {
+  return [].reduce.call(
+    elements,
+    (data, element) => {
+      data[element.name] = element.value;
+      return data;
+    },
+    {}
+  );
+}
+
+function handleSubmit(event) {
+  //  const form = document.querySelector("#testform")[0];
+  const form = document.getElementById("testform");
+  const data = formToJSON(form.elements);
+  // event.preventDefault();
+  const r = JSON.stringify(data);
+  console.log(r);
+  window.alert(r);
+}
+
+setTimeout(console.log("Haalo"), 500);
+
+window.onload = function () {
+  console.log("start");
+  form.addEventListener("submit", handleSubmit);
+};
