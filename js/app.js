bb265b1cd7386cb47cd8f7c0ff252a66521bb57f
uwek <Uwe.Kallmeyer@gmail.com>
2021-04-09 11:49:00 +0200

    chg

---
+++ js/app.js
@@ -0,0 +1,19 @@
+/*
+Was will ich wissen
+
+Unternehmen
+  Name
+  PLZ, Ort
+  Ansprechpartner, Email
+  Anz. MA
+  Anz. User
+  Branche
+  Produkte
+  Rechtsform, Inhaberstruktur
+  Altsystem
+
+Opportunity
+  Entscheidung bis
+
+
+*/
